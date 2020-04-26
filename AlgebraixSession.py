86a516e90b5697ed8ec42874d55e730d933c8d1f
Gragt <francois.grandjean@gmail.com>
2020-04-25 20:01:09 -0500

    Create AlgebraixSession.py

---
+++ AlgebraixSession.py
@@ -0,0 +1,14 @@
+import os
+
+from selenium import webdriver
+
+
+def downloadAlgebraixInbox():
+    """
+    Opens a web browser through Selenium and navigates through the Algebraix
+    inbox, for each message creating a folder in ~/Downloads/ named after the
+    sender and saving all attachments in it.
+    Inputs: none.
+    Returns: nothing.
+    """
+    pass
