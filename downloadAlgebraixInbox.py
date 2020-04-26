aa4a0b234bae9c2b08f58434a87d6a3f291264ae
Gragt <francois.grandjean@gmail.com>
2020-04-26 14:15:50 -0500

    Create downloadAlgebraixInbox.py

---
+++ downloadAlgebraixInbox.py
@@ -0,0 +1,40 @@
+import time
+
+from AlgebraixSession import AlgebraixSession
+
+
+def downloadAlgebraixInbox():
+    """
+    Create a session to log into Algebraix and download inbox’s contents.
+    Requires the user to log in and navigate to the first message. Closes the
+    browser at the end.
+    Returns: nothing.
+    """
+    print("Opening web browser …")
+    session = AlgebraixSession()
+    print("Web browser open. Please, manually log into Algebraix.")
+    input("Open the first message and press Enter to continue.")
+    next = session.findNext()
+    while True:
+        time.sleep(1.5)
+        print("Getting name …")
+        session.setSenderName()
+        print(session.senderName)
+        print("Getting body text …")
+        session.setBodyText()
+        print("Getting attchments …")
+        session.setAttachments()
+        print("Creating directories …")
+        session.createDownloadDirectory()
+        print("Downloading files …")
+        session.downloadFiles()
+        next = session.findNext()
+        if not next:
+            print("All done.")
+            break
+        print("Moving on to next message …")
+        next.click()
+    session.browserClose()
+
+
+downloadAlgebraixInbox()
