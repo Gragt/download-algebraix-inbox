d777184ce81e24c75589c85842af054e80f649b1
Gragt <francois.grandjean@gmail.com>
2020-04-25 20:27:28 -0500

    Implement getSenderName

--- AlgebraixSession.py
+++ AlgebraixSession.py
@@ -2,27 +2,33 @@ import os
 
 from selenium import webdriver
 
+"""
+Opens a web browser through Selenium and navigates through the Algebraix
+inbox, for each message creating a folder in ~/Downloads/ named after the
+sender and saving all attachments in it.
+Inputs: none.
+Returns: nothing.
+"""
 
-def downloadAlgebraixInbox():
+
+def openWebBrowser():
     """
-    Opens a web browser through Selenium and navigates through the Algebraix
-    inbox, for each message creating a folder in ~/Downloads/ named after the
-    sender and saving all attachments in it.
+    Opens the web browser and go to the Algebraix homepage.
     Inputs: none.
-    Returns: nothing.
-    """
-    def openWebBrowser():
-        """
-        Opens the web browser and go to the Algebraix homepage.
-        Inputs: none.
-        Returns: none.
-        """
-        browser = webdriver.Firefox()
-        browser.get('https://c1-liceodelvalle.algebraix.com/')
-
+    Returns: none.
     """
     print("Opening web browser …")
-    openWebBrowser()
+    browser = webdriver.Firefox()
+    browser.get('https://c1-liceodelvalle.algebraix.com/')
     print("Web browser open. Please, manually log into Algebraix.")
-    print("All done.")
+    return browser
+
+
+def getSenderName(browser):
     """
+    Finds and returns sender’s name.
+    Inputs: none.
+    Returns: a string.
+    """
+    return browser.find_element_by_class_name(
+        'material-card__text--primary').text
