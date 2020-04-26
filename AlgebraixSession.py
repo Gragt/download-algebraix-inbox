42a557fd83f662cb5acdb83b178891757d1dfad7
Gragt <francois.grandjean@gmail.com>
2020-04-25 20:46:10 -0500

    Restructure program by implementing a class

--- AlgebraixSession.py
+++ AlgebraixSession.py
@@ -2,33 +2,25 @@ import os
 
 from selenium import webdriver
 
-"""
-Opens a web browser through Selenium and navigates through the Algebraix
-inbox, for each message creating a folder in ~/Downloads/ named after the
-sender and saving all attachments in it.
-Inputs: none.
-Returns: nothing.
-"""
 
+class AlgebraixSession(object):
+    """
+    Opens a web browser through Selenium and navigates through the Algebraix
+    inbox, for each message creating a folder in ~/Downloads/ named after the
+    sender and saving all attachments in it.
+    """
 
-def openWebBrowser():
-    """
-    Opens the web browser and go to the Algebraix homepage.
-    Inputs: none.
-    Returns: none.
-    """
-    print("Opening web browser …")
-    browser = webdriver.Firefox()
-    browser.get('https://c1-liceodelvalle.algebraix.com/')
-    print("Web browser open. Please, manually log into Algebraix.")
-    return browser
+    def __init__(self):
+        """
+        Initialises the session by opening the web browser then waits.
+        """
+        self.browser = webdriver.Firefox()
+        self.browser.get('https://c1-liceodelvalle.algebraix.com/')
 
-
-def getSenderName(browser):
-    """
-    Finds and returns sender’s name.
-    Inputs: none.
-    Returns: a string.
-    """
-    return browser.find_element_by_class_name(
-        'material-card__text--primary').text
+    def getSenderName(self):
+        """
+        Finds and returns sender’s name.
+        Returns: a string.
+        """
+        return self.browser.find_element_by_class_name(
+            'material-card__text--primary').text
